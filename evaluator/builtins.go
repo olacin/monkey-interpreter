3272dc8f7839a727da53bad558911c79f5a7c98a
olacin <olacin@users.noreply.github.com>
2024-09-30 20:06:27 +0200

    feat: complete chapter 4

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/olacin/monkey-interpreter/object"
@@ -113,6 +114,15 @@ var builtins = map[string]*object.Builtin{
 			default:
 				return newError("argument to `push` not supported, got %s", arg.Type())
 			}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
